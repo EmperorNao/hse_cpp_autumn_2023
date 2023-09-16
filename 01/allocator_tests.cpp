9761f1e6936c8978300b950c7018758b2ea5a57a
emperornao <ansicpp2020@gmail.com>
2023-09-16 04:03:50 +0300

    Add tests

--- 01/allocator_tests.cpp
+++ 01/allocator_tests.cpp
@@ -1,7 +1,6 @@
 #include "allocator_tests.hpp"
 
 
-
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
--- 01/allocator_tests.hpp
+++ 01/allocator_tests.hpp
@@ -1,3 +1,5 @@
+#include <limits>
+
 #include "gtest/gtest.h"
 
 #include "allocator.hpp"
@@ -6,8 +8,66 @@
 class TestAllocator: public ::testing::Test {
 
 protected:
-    void setUp() {}
-    void tearDown() {}
-
     Allocator allocator;
+
+    void setUp() { allocator = Allocator(); }
+    void tearDown() { allocator.~Allocator(); }
+
 };
+
+
+TEST_F(TestAllocator, testEmptyAllocator) {
+    ASSERT_EQ(nullptr, allocator.alloc(1));
+}
+
+TEST_F(TestAllocator, testAllocatorAlloc) {
+    allocator.makeAllocator(10);
+    ASSERT_NE(nullptr, allocator.alloc(10));
+}
+
+TEST_F(TestAllocator, testAllocatorAllocBig) {
+    unsigned long bigNumber = std::numeric_limits<int>::max();
+    allocator.makeAllocator(bigNumber + 1);
+    ASSERT_NE(nullptr, allocator.alloc(bigNumber));
+}
+
+TEST_F(TestAllocator, testAllocatorAllocChunks) {
+    allocator.makeAllocator(10);
+    allocator.alloc(5);
+    ASSERT_NE(nullptr, allocator.alloc(5));
+}
+
+TEST_F(TestAllocator, testAllocatorAllocOverflow) {
+    allocator.makeAllocator(10);
+    allocator.alloc(10);
+    ASSERT_EQ(nullptr, allocator.alloc(1));
+}
+
+TEST_F(TestAllocator, testAllocatorReset) {
+    allocator.makeAllocator(10);
+    allocator.alloc(10);
+    allocator.reset();
+    ASSERT_NE(nullptr, allocator.alloc(10));
+}
+
+TEST_F(TestAllocator, testAllocatorResetChunks) {
+    allocator.makeAllocator(10);
+    allocator.alloc(10);
+    allocator.reset();
+    allocator.alloc(5);
+    ASSERT_NE(nullptr, allocator.alloc(5));
+}
+
+TEST_F(TestAllocator, testAllocatorRecreationIncreasing) {
+    allocator.makeAllocator(10);
+    allocator.alloc(10);
+    allocator.makeAllocator(15);
+    ASSERT_NE(nullptr, allocator.alloc(15));
+}
+
+TEST_F(TestAllocator, testAllocatorRecreationDecreasing) {
+    allocator.makeAllocator(10);
+    allocator.alloc(10);
+    allocator.makeAllocator(5);
+    ASSERT_EQ(nullptr, allocator.alloc(15));
+}
