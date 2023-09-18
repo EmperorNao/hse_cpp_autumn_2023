88b4d082216b20f20b1428912d99cba2c639eb9e
emperornao <ansicpp2020@gmail.com>
2023-09-19 01:01:10 +0300

    Add correct SetUp and TearDown with allocator creation

--- 01/allocator_tests.hpp
+++ 01/allocator_tests.hpp
@@ -8,66 +8,66 @@
 class TestAllocator: public ::testing::Test {
 
 protected:
-    Allocator allocator;
+    Allocator* allocator;
 
-    void setUp() { }
-    void tearDown() { }
+    void SetUp() { allocator = new Allocator(); }
+    void TearDown() { delete allocator; }
 
 };
 
 
 TEST_F(TestAllocator, testEmptyAllocator) {
-    ASSERT_EQ(nullptr, allocator.alloc(1));
+    ASSERT_EQ(nullptr, allocator->alloc(1));
 }
 
 TEST_F(TestAllocator, testAllocatorAlloc) {
-    allocator.makeAllocator(10);
-    ASSERT_NE(nullptr, allocator.alloc(10));
+    allocator->makeAllocator(10);
+    ASSERT_NE(nullptr, allocator->alloc(10));
 }
 
 TEST_F(TestAllocator, testAllocatorAllocBig) {
     unsigned long bigNumber = std::numeric_limits<int>::max();
-    allocator.makeAllocator(bigNumber + 1);
-    ASSERT_NE(nullptr, allocator.alloc(bigNumber));
+    allocator->makeAllocator(bigNumber + 1);
+    ASSERT_NE(nullptr, allocator->alloc(bigNumber));
 }
 
 TEST_F(TestAllocator, testAllocatorAllocChunks) {
-    allocator.makeAllocator(10);
-    allocator.alloc(5);
-    ASSERT_NE(nullptr, allocator.alloc(5));
+    allocator->makeAllocator(10);
+    allocator->alloc(5);
+    ASSERT_NE(nullptr, allocator->alloc(5));
 }
 
 TEST_F(TestAllocator, testAllocatorAllocOverflow) {
-    allocator.makeAllocator(10);
-    allocator.alloc(10);
-    ASSERT_EQ(nullptr, allocator.alloc(1));
+    allocator->makeAllocator(10);
+    allocator->alloc(10);
+    ASSERT_EQ(nullptr, allocator->alloc(1));
 }
 
 TEST_F(TestAllocator, testAllocatorReset) {
-    allocator.makeAllocator(10);
-    allocator.alloc(10);
-    allocator.reset();
-    ASSERT_NE(nullptr, allocator.alloc(10));
+    allocator->makeAllocator(10);
+    allocator->alloc(10);
+    allocator->reset();
+    ASSERT_NE(nullptr, allocator->alloc(10));
 }
 
 TEST_F(TestAllocator, testAllocatorResetChunks) {
-    allocator.makeAllocator(10);
-    allocator.alloc(10);
-    allocator.reset();
-    allocator.alloc(5);
-    ASSERT_NE(nullptr, allocator.alloc(5));
+    allocator->makeAllocator(10);
+    allocator->alloc(10);
+    allocator->reset();
+    allocator->alloc(5);
+    ASSERT_NE(nullptr, allocator->alloc(5));
 }
 
 TEST_F(TestAllocator, testAllocatorRecreationIncreasing) {
-    allocator.makeAllocator(10);
-    allocator.alloc(10);
-    allocator.makeAllocator(15);
-    ASSERT_NE(nullptr, allocator.alloc(15));
+    allocator->makeAllocator(10);
+    allocator->alloc(10);
+    allocator->makeAllocator(15);
+    ASSERT_NE(nullptr, allocator->alloc(15));
 }
 
 TEST_F(TestAllocator, testAllocatorRecreationDecreasing) {
-    allocator.makeAllocator(10);
-    allocator.alloc(10);
-    allocator.makeAllocator(5);
-    ASSERT_EQ(nullptr, allocator.alloc(15));
+    allocator->makeAllocator(10);
+    allocator->alloc(10);
+    allocator->makeAllocator(5);
+    ASSERT_EQ(nullptr, allocator->alloc(15));
 }
